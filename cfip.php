823b8f5c763b8bc8c0071686fb6c080317f82730
suancaicc <159261812+suancaicc@users.noreply.github.com>
2025-01-07 17:24:44 +0800

    Update cfip.php

--- cfip.php
+++ cfip.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 // 要发送的数据
 $data = array('key' => 'iDetkOys');
 
@@ -7,38 +8,128 @@ $json_data = curl($data);
 
 // 检查$json_data是否是一个数组
 if (is_array($json_data) && isset($json_data['info'])) {
-    // 打开一个文件用于写入
-    $file = fopen('cfip.txt', 'w');
-
     // 遍历数组中的每个元素，提取ip和line字段并写入文件
     foreach ($json_data['info'] as $info) {
         //$txt .= $info['ip'] . "\n";
-
-    // 根据 line 的值进行替换
-    if ($info['line'] == 'CM') {
-        $txt = $txt.$info['ip'] . '#移动' . "<br>";
-        $output = $info['ip'] . '#移动' . "\n";
-    } elseif ($info['line'] == 'CU') {
-        $txt =$txt. $info['ip'] . '#联通' . "<br>" ;
-        $output = $info['ip'] . '#联通' . "\n";
-    }elseif ($info['line'] == 'CT') {
-        $txt = $txt.$info['ip'] . '#电信' . "<br>" ;
-        $output = $info['ip'] . '#电信' . "\n";
-    } else {
-        //echo $info['line'] . PHP_EOL . "\n"; // 如果没有匹配，上输出原值
-        //$txt = $info['ip'] . '#' . $info['ip'] . PHP_EOL . "\n";
+        $content = '';
+        // 根据 line 的值进行替换
+        if ($info['line'] == 'CM') {
+            $txt .= $txt . $info['ip'] . '#移动' . "<br>";
+            $content .= $info['ip'] . '#移动' . "\r\n";
+        } elseif ($info['line'] == 'CU') {
+            $txt .= $txt . $info['ip'] . '#联通' . "<br>";
+            $content .= $info['ip'] . '#联通' . "\r\n";
+        } elseif ($info['line'] == 'CT') {
+            $txt .= $txt . $info['ip'] . '#电信' . "<br>";
+            $content .= $info['ip'] . '#电信' . "\r\n";
+        } else {
+            $txt .= $txt . $info['ip'] . '#' . $info['ip'] . "<br>";
+            $content .= $info['ip'] . '#' . $info['ip'] . "\r\n";
+        }
     }
-    fwrite($file, $output);
-}
-exit($txt);
-echo $txt;
+    //exit($txt);
 
-    // 关闭文件
-    fclose($file);
 } else {
     echo "错误：从API获取的数据不是一个有效的数组。";
 }
 
+
+// 使用示例
+$token = ' '; // 在这里放入你的 GitHub 令牌
+$repo = 'suancaicc/cf-ip'; // 例如 'username/repo'
+$branch = 'main'; // 你要提交的分支
+$filePath = 'ip.txt'; // 要创建或更新的文件路径
+//$content = "这是文件的内容"; // 文件内容
+$commitMessage = "提交或更新文件"; // 提交的信息
+
+// 调用函数
+$git_post = githubPutFile($repo, $branch, $filePath, $content, $token, $commitMessage);
+//exit($git_post);
+// 将JSON字符串转换为PHP数组
+$msg = json_decode($git_post, true);
+// 获取commit的message
+$message = $msg['commit']['message'];
+
+// 输出响应
+if ($message == "提交或更新文件") {
+    echo "文件提交成功：" . $message;
+} else {
+    echo "文件提交失败，状态码：" . $msg['status'] . "</br>\r\n";
+    echo "响应内容：$git_post";
+}
+/**
+ * 将文件提交到GitHub仓库
+ * 
+ * @param string $repo GitHub仓库名称，格式为'username/repo'
+ * @param string $branch 要提交的分支名称
+ * @param string $filePath 要创建或更新的文件路径
+ * @param string $content 要提交的文件内容
+ * @param string $token GitHub API令牌
+ * @param string $commitMessage 提交的信息
+ */
+function githubPutFile($repo, $branch, $filePath, $content, $token, $commitMessage) {
+    // 先获取文件的SHA值（如果文件已存在）
+    $url = "https://api.github.com/repos/$repo/contents/$filePath?ref=$branch";
+    $options = [
+        'http' => [
+            'header' => [
+                "Authorization: token $token",
+                "User-Agent: PHP-Script",
+                "Accept: application/vnd.github.v3+json"
+            ]
+        ]
+    ];
+    
+    // 创建HTTP上下文
+    $context = stream_context_create($options);
+
+    $response = file_get_contents($url, false, $context);
+
+    $responseData = json_decode($response, true);
+  
+    // 获取文件的SHA值
+    $file_sha = isset($responseData['sha']) ? $responseData['sha'] : '';
+
+    // 将内容进行base64编码
+    $base64_content = base64_encode($content);
+    
+    // 创建提交的JSON数据
+    $data = [
+        'message' => $commitMessage,
+        'branch' => $branch,
+        'content' => $base64_content,
+    ];
+    
+    // 如果文件已存在，则加入SHA值
+    if ($file_sha) {
+        $data['sha'] = $file_sha;
+    }
+
+    // 提交文件
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/repos/$repo/contents/$filePath");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: token $token",
+        "User-Agent: PHP-Script",
+        "Content-Type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+    // 执行请求
+    $response = curl_exec($ch);
+    curl_close($ch);
+    // 输出响应
+    return $response;
+}
+
+/**
+ * 发送POST请求到指定URL并获取JSON数据
+ * 
+ * @param array $data 要发送的数据
+ * @return array|null 返回解析后的JSON数据，如果出错则返回null
+ */
 function curl($data)
 {
     // 将数据转换为JSON格式
@@ -51,9 +142,13 @@ function curl($data)
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        'Content-Length: ' . strlen($json_data))
+    curl_setopt(
+        $ch,
+        CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($json_data)
+        )
     );
 
     // 执行cURL会话并获取响应
@@ -79,4 +174,5 @@ function curl($data)
 
     return $decoded_data;
 }
+
 ?>
